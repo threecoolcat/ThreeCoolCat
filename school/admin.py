2cd5b011399c703360570b1c74c68c83ec670605
anyi <cnanyi@qq.com>
2020-06-25 01:01:04 +0800

    学校，课程 关系 admin

--- school/admin.py
+++ school/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import School
+from .models import School, Course
 # Register your models here.
 
 
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'linkman', 'phone')
+    search_fields = ('name', 'address', 'linkman', 'phone')
+    pass
+
+
+@admin.register(Course)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('name', 'intro', 'enabled')
     pass
--- school/models.py
+++ school/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class CourseInSchool(models.Model):
+    id = models.AutoField(primary_key=True)
+    school = models.ForeignKey('school.School', on_delete=models.CASCADE, verbose_name='校区', null=True, blank=True, related_name='schools', db_column='school_id')
+    course = models.ForeignKey('school.Course', on_delete=models.CASCADE, verbose_name='课程', null=True, blank=True, related_name='courses', db_column='course_id')
 
 
 class School(models.Model):
@@ -15,11 +19,31 @@ class School(models.Model):
     phone = models.CharField('联系电话', db_column='phone', null=True, blank=True, max_length=255)
     order_by = models.IntegerField('排序', db_column='order_by', null=True, blank=True, default=0)
 
+    courses = models.ManyToManyField('Course', verbose_name='课程',
+                                     through='CourseInSchool', through_fields=('school', 'course'))
+
     class Meta:
         managed = True
         db_table = 'school'
         verbose_name = '校区'
         verbose_name_plural = '校区管理'
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField('名称', db_column='name', null=False, blank=False, default='', max_length=255)
+    intro = models.TextField('简介', db_column='intro', null=True, blank=True)
+    enabled = models.BooleanField('启用', db_column='enabled', null=False, blank=False, default=True)
+    order_by = models.IntegerField('排序', db_column='order_by', null=True, blank=True, default=0)
+
+    class Meta:
+        managed = True
+        db_table = 'course'
+        verbose_name = '课程'
+        verbose_name_plural = '课程管理'
 
     def __str__(self):
         return self.name
