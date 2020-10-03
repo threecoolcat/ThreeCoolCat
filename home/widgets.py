952207ae5e7a52762174e782be1a2c8e457092a9
anyi <cnanyi@qq.com>
2020-10-03 11:33:08 +0800

    增加新闻模块

--- home/widgets.py
+++ home/widgets.py
@@ -29,39 +29,3 @@ laydate({{
 </script>""".format(name=name, mintime="min: '" + self.mintime.strftime("%Y-%m-%d") + "'," if self.mintime else "")
         return out
 
-
-class FineUploadGalleryWidget(Widget):
-    @property
-    def media(self):
-        js = ["fine-uploader/5.14.1/fine-uploader.min.js", "fine-uploader/uploadhelper.js"]
-        css = {"all": ("fine-uploader/5.14.1/fine-uploader-gallery.css",)}
-        return Media(js=["%s" % path for path in js], css=css)
-
-    def render(self, name, value, attrs=None, renderer=None):
-        return """<input type="hidden" id="id_{name}" name="{name}" value="{value}" />
-<div id="uploader-{name}" class="col-md-12" style="{style}"></div>
-<script type="text/javascript">
-uploadhelper.config("{name}", "picture", "/common/upload/session/?url={value}", {multiple});
-</script>
-        """.format(name=name, value=value, style=self.attrs['style'] if 'style' in self.attrs else '',
-                   multiple=self.attrs['multiple'] if 'multiple' in self.attrs else 'false')
-
-
-class FineUploadThumbnailWidget(Widget):
-    @property
-    def media(self):
-        js = ["fine-uploader/5.14.1/fine-uploader.min.js", "fine-uploader/uploadhelper.js"]
-        css = {"all": ("fine-uploader/5.14.1/fine-uploader-new.css",)}
-        return Media(js=["%s" % path for path in js], css=css)
-
-    def render(self, name, value, attrs=None, renderer=None):
-        disabled = 'true' if 'disabled' in attrs and attrs['disabled'] else 'false'
-        return """<input type="hidden" id="id_{name}" name="{name}" value="{value}" />
-<input type="hidden" id="{name}_count" name="{name}_count" value="0" />
-<div id="uploader-{name}" class="col-md-12" style="{style}"></div>
-<script type="text/javascript">
-uploadhelper.config("{name}", "{type}", "{value}", {multiple}, {disabled});
-</script>
-        """.format(name=name, value=value, style=self.attrs['style'] if 'style' in self.attrs else '',
-                   multiple=self.attrs['multiple'] if 'multiple' in self.attrs else 'false', type=self.attrs['type'],
-                   disabled=disabled)
--- school/forms.py
+++ school/forms.py
@@ -1,12 +1,9 @@
 from django import forms
 import datetime
-from home.widgets import LayDateWidget, FineUploadThumbnailWidget
+from home.widgets import LayDateWidget
 
 
 class CourseForm(forms.ModelForm):
     start_date = forms.DateField(label='开课日期', required=False,
                                  widget=LayDateWidget(attrs={'style': 'width:120px', 'readonly': 'true'}),
                                  initial=datetime.date.today())
-    # cover = forms.CharField(label='封面', max_length=500, required=False,
-    #                         widget=FineUploadThumbnailWidget(
-    #                             attrs={'title': '', 'multiple': 'false', 'type': 'jpg'}))
