dc2f99165f81780bbeaa445abd83815085b8e6b7
anyi <cnanyi@qq.com>
2021-03-03 23:23:09 +0800

    增加测试方法， 增加测试需要的配置项

--- ThreeCoolCat/settings.py
+++ ThreeCoolCat/settings.py
@@ -145,6 +145,7 @@ DATABASES = {
         # 'PASSWORD': 'www.isoftstone.CoM',
         # 服务器地址
         'HOST': '127.0.0.1',
+        'CHARSET': 'utf8mb4',
         'TEST': {
             'NAME': 'threecoolcat_test',
             'CHARSET': 'utf8mb4'
